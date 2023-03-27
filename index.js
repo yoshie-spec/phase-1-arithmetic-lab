85970320408582cc858d2ba56969d8eb0c4a7443
yosselin alvarez <yoshie_9311@icloud.com>
2023-03-26 21:37:19 -0400

    complete lap

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random =Math.floor(Math.random() * 8) + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+const max = Math.max(1, 2, 3, 4, 5, 17, 20);
